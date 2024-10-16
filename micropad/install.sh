44bcecbc705f98bcf7ba60fde8fd0b282289606e
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-10-16 16:57:33 +0300

    Update install.sh

--- micropad/install.sh
+++ micropad/install.sh
@@ -1,7 +1,7 @@
 sudo apt install git -y
 git clone https://github.com/YigitC7/Micropad
 cd Micropad/deb-pack
-sudo dpkg -i micopad_1.0.deb
+sudo dpkg -i micropad_1.0.deb
 echo "=================================="
 echo "Kurulum tamamlandı"
 cd ..
