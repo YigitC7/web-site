6b149969dbac630280970e242b823ee61fc0bdcb
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-10-28 16:08:21 +0300

    Update install.sh

--- microhesapmakinesi/install.sh
+++ microhesapmakinesi/install.sh
@@ -1,6 +1,6 @@
 sudo apt install git -y
 git clone https://github.com/YigitC7/MicroHesapMakinesi
-sudo dpkg -i MicroHesapMakinesi/deb-pack/microhesapmakinesi-1.0.deb
+sudo dpkg -i MicroHesapMakinesi/deb-pack/microhesapmakinesi-1.1.deb
 echo "=================================="
 echo "Kurulum tamamlandı"
 rm -r MicroHesapMakinesi
