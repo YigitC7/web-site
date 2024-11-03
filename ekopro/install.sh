b3afcd4aa32b527861a5bbcae9b1bf15960ee34d
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-11-03 15:12:23 +0300

    Update install.sh

--- ekopro/install.sh
+++ ekopro/install.sh
@@ -1,6 +1,6 @@
 sudo apt install git -y
 git clone https://github.com/YigitC7/Ekopro
-sudo dpkg -i Ekopro/deb-pack/ekopro-1.0.deb
+sudo dpkg -i Ekopro/deb-pack/ekopro.deb
 echo "=================================="
 echo "Kurulum tamamlandı"
 rm -r Ekopro
