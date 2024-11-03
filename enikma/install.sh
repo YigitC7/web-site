5ed98b458be7982c0595519e27dff723a27d7a81
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-11-03 15:11:58 +0300

    Update install.sh

--- enikma/install.sh
+++ enikma/install.sh
@@ -1,6 +1,6 @@
 sudo apt install git -y
 git clone https://github.com/YigitC7/enikma
-sudo dpkg -i enikma/deb-pack/enikma-1.0.deb
+sudo dpkg -i enikma/deb-pack/enikma.deb
 echo "=================================="
 echo "Kurulum tamamlandı"
 rm -r enikma
