bf2b31c41274dd3320e4b23a259fb262a40b0133
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-12-22 18:39:08 +0300

    Update install.sh

--- Defter/install.sh
+++ Defter/install.sh
@@ -1,5 +1,5 @@
 sudo dnf install git -y
-git clone https://github.com/YigitC7/Ekopro
+git clone https://github.com/YigitC7/Defter
 sudo rpm -ivh Defter/defter_1.0_x64.rpm
 echo "=================================="
 echo "Kurulum tamamlandı"
