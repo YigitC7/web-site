3128ea0e6067fb81a3415a196896b7696a7bf702
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-12-22 18:31:27 +0300

    Create install.sh

---
+++ Defter/install.sh
@@ -0,0 +1,6 @@
+sudo dnf install git -y
+git clone https://github.com/YigitC7/Ekopro
+sudo rpm -ivh Defter/defter_1.0_x64.rpm
+echo "=================================="
+echo "Kurulum tamamlandı"
+rm -r Defter
