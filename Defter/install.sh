d00030092ea3e01226246c58d63653ac85b82adf
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-12-23 13:45:20 +0300

    Update install.sh

--- Defter/install.sh
+++ Defter/install.sh
@@ -1,6 +1,6 @@
 sudo dnf install git -y
 git clone https://github.com/YigitC7/Defter
-sudo rpm -ivh Defter/defter_1.0_x64.rpm
+sudo rpm -ivh Defter/defter.rpm
 echo "=================================="
 echo "Kurulum tamamlandı"
 rm -r Defter
