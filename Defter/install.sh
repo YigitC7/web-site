eb89a70f932439a6cdf4a1ab7be885b91fce3c93
Yiğit Çıtak <yigitcitak007@gmail.com>
2024-12-31 18:10:48 +0300

    Update install.sh

--- Defter/install.sh
+++ Defter/install.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 sudo dnf install git -y
 git clone https://github.com/YigitC7/Defter
 sudo rpm -ivh Defter/defter.rpm
 echo "=================================="
+
+read -p "defter-guncelle"
+read -p "curl -sSL https://yigit-packages.netlify.app/Defter/upgrade.sh | bash"
+
+sudo bash -c "echo '#!/bin/bash' > /usr/local/bin/$betik_adi"
+sudo bash -c "echo '$komut' >> /usr/local/bin/$betik_adi"
+
+sudo chmod +x /usr/local/bin/$betik_adi
+
 echo "Kurulum tamamlandı"
 rm -r Defter
