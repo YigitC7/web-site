c18a53ca14338c8703d84245534bfd36c62e55e6
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-01-03 21:30:35 +0300

    Update upgrade.sh

--- Defter/upgrade.sh
+++ Defter/upgrade.sh
@@ -1,6 +1,6 @@
 sudo dnf install git -y
 git clone https://github.com/YigitC7/Defter
-sudo rpm -ivh Defter/defter.rpm
+sudo rpm -Uvh Defter/defter.rpm
 echo "=================================="
 echo "Güncelleme işlemi bitti"
 rm -r Defter
