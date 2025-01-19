f42d58ba8b3446736371a3bd9098eadc172e6c36
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-01-19 16:43:23 +0300

    Update install.sh

--- Defter/deb/install.sh
+++ Defter/deb/install.sh
@@ -1,6 +1,6 @@
 sudo apt install git -y
 git clone https://github.com/YigitC7/Defter
-sudo dpkg -i MicroHesapMakinesi/deb/Defter.deb
+sudo dpkg -i Defter/deb/Defter.deb
 echo "=================================="
 echo "Kurulum tamamlandı"
-rm -r MicroHesapMakinesi
+rm -r Defter
